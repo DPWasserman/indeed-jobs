ce83a368093d8f81a12a47e3bb7ba3dae18732f6
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-07 12:31:24 -0500

    Set USER_AGENT

--- indeed/settings.py
+++ indeed/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = 'indeed.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'indeed (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.146 Safari/537.36'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
