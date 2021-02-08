c7afc9b9474c590d3b672f485855b35477291476
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-08 08:42:06 -0500

    Changed throttling settings

--- indeed/settings.py
+++ indeed/settings.py
@@ -25,9 +25,9 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0.1 # Set to an appropriate number
+DOWNLOAD_DELAY = 0.5 # Set to an appropriate number
 # The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
