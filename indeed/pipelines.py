9953843ed46c1ea9db7c919eefcc7dff70e6a624
David Wasserman <davidphilipwasserman@gmail.com>
2021-02-09 19:47:41 -0500

    Cleaned out unused code

--- indeed/pipelines.py
+++ indeed/pipelines.py
@@ -9,56 +9,9 @@
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
 from scrapy.exporters import CsvItemExporter
-import pandas as pd
-import pymssql
-import re
 
-# class IndeedPipeline:
-#     def process_item(self, item, spider):
-#         return item
 
 filename = 'indeed.csv'
-
-
-class CleansePipeline(object):
-    def __init__(self):
-        pass
-
-    def process_item(self, item, spider):
-        adapter = ItemAdapter(item)
-        try:
-            adapter['num_stars'], _, adapter['num_reviews'] = re.findall('([/d.]+)', adapter['company_reviews'])
-        except:
-            adapter['num_stars'] = None
-            adapter['num_reviews'] = None
-
-        if adapter['posted_when'] in ['Today', 'Just posted']:
-            day_diff = 0
-        else:
-            try:
-                day_diff = re.findall('/d+', adapter['posted_when'])
-            except:
-                day_diff = None
-
-
-class DataPipeline(object):
-    def __init__(self):
-        self.conn = pymssql.connect(host='127.0.0.1', user='', password='', database='job_scraper')
-        self.cursor = self.conn.cursor()
-        self.filename = filename
-
-    def process_item(self, item, spider):
-        try:
-            df = pd.read_csv(self.filename)
-            company_df =  df[['company_name','company_url','num_stars','num_reviews']].unique()
-            
-            sql_statement = f'INSERT INTO jobs() VALUES ({item["text"]}, {item[""]}, {item[""]})'
-            self.cursor.execute(sql_statement)
-            self.conn.commit()
-        except pymssql.Error as e:
-            print ("error")
-
-        return item
 
 
 class DuplicatesPipeline(object):
